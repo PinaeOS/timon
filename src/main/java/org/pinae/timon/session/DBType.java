0dfe0c791cc875c46c7b8e8e8fc4cb4eab00fa68
Yugeng Hui <interhuiyg@163.com>
2015-08-14 01:43:04 +0800

    Add DBType

---
+++ src/main/java/org/pinae/timon/session/DBType.java
@@ -0,0 +1,15 @@
+package org.pinae.timon.session;
+
+/**
+ * 数据库类型
+ * 
+ * @author huiyugeng
+ *
+ */
+public class DBType {
+	public final static String ORACLE = "oracle";
+	
+	public final static String MYSQL = "mysql";
+	
+	public final static String SQLITE = "sqlite";
+}
