6ccd7b9d704d6dac8e52c5d13a8a75d128f6921d
Yugeng Hui <interhuiyg@163.com>
2015-08-21 00:14:53 +0800

    Update SqlSession Unit test case

--- src/test/java/org/pinae/timon/session/SqlSessionFactoryTest.java
+++ src/test/java/org/pinae/timon/session/SqlSessionFactoryTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.junit.Test;
+import org.pinae.timon.session.defaults.DefaultSqlSessionFactory;
+import org.pinae.timon.session.handle.ConnectionHandler;
 
 public class SqlSessionFactoryTest {
 
@@ -14,7 +18,7 @@ public class SqlSessionFactoryTest {
 		SqlSessionFactory sessionFactory = null;
 		
 		try {
-			sessionFactory = new SqlSessionFactory();
+			sessionFactory = new DefaultSqlSessionFactory();
 			
 			SqlSession session = sessionFactory.getSession();
 			assertEquals(session.isClosed(), false);
@@ -22,6 +26,38 @@ public class SqlSessionFactoryTest {
 			session.close();
 				
 		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testGetSessionWithHandler() {
+		ConnectionHandler handler = new ConnectionHandler() {
+			public void handle(Connection connection) {
+				try {
+					connection.setAutoCommit(false);
+				} catch (SQLException e) {
+					fail(e.getMessage());
+				}
+			}
+		};
+		
+		SqlSessionFactory sessionFactory = null;
+		
+		try {
+			sessionFactory = new DefaultSqlSessionFactory();
+			
+			SqlSession session = sessionFactory.getSession(handler);
+			assertEquals(session.isClosed(), false);
+			
+			Connection connection = session.getConnection();
+			assertEquals(connection.getAutoCommit(), false);
+				
+			session.close();
+				
+		} catch (IOException e) {
+			fail(e.getMessage());
+		} catch (SQLException e) {
 			fail(e.getMessage());
 		}
 	}
--- src/test/java/org/pinae/timon/session/SqlSessionTest.java
+++ src/test/java/org/pinae/timon/session/SqlSessionTest.java
@@ -14,6 +14,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.pinae.timon.session.defaults.DefaultSqlSessionFactory;
+import org.pinae.timon.session.handle.ResultHandler;
 import org.pinae.timon.session.pojo.AnnotationPerson;
 import org.pinae.timon.session.pojo.Person;
 import org.pinae.timon.sql.SqlBuilder;
@@ -31,7 +33,7 @@ public class SqlSessionTest {
 	public static void init() {
 		try {
 			SqlSessionTest.builder = new SqlBuilder();
-			SqlSessionTest.sessionFactory = new SqlSessionFactory();
+			SqlSessionTest.sessionFactory = new DefaultSqlSessionFactory();
 		} catch (IOException e) {
 			fail(e.getMessage());
 		}
@@ -73,6 +75,41 @@ public class SqlSessionTest {
 		parameters.put("id", 1);
 		AnnotationPerson person = (AnnotationPerson)session.one(builder.getSQLByNameWithParameters("GET_PERSON_2", parameters), AnnotationPerson.class);
 		assertNotNull(person.getUserName());
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testSelectWithHandler() {
+		ResultHandler handler = new ResultHandler() {
+			@SuppressWarnings("rawtypes")
+			public <T> void handle(T t) {
+				if (t instanceof List) {
+					List dataList = (List)t;
+					if (dataList.size() == 3) {
+						dataList.remove(0);
+					}
+				}
+			}
+		};
+		List<Map<String, Object>> table = (List<Map<String, Object>>) session.select(builder.getSQLByName("GET_ID"), 
+				Map.class, handler);
+		assertEquals(table.size(), 2);
+	}
+	
+	@Test
+	public void testOneForAnnotationWithHandler() {
+		ResultHandler handler = new ResultHandler() {
+			public <T> void handle(T t) {
+				if (t instanceof AnnotationPerson) {
+					AnnotationPerson person = (AnnotationPerson)t;
+					person.setUserName("Timon");
+				}
+			}
+		};
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("id", 1);
+		AnnotationPerson person = (AnnotationPerson)session.one(builder.getSQLByNameWithParameters("GET_PERSON_2", parameters), 
+				AnnotationPerson.class, handler);
+		assertEquals(person.getUserName(), "Timon");
 	}
 	
 	@After
