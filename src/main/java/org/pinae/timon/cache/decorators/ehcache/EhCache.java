e4dc9347d0631a3b4e8a938b7fcb69ef67cc43ce
Yugeng Hui <interhuiyg@163.com>
2015-10-29 10:41:42 +0800

    Support Ehcache

---
+++ src/main/java/org/pinae/timon/cache/decorators/ehcache/EhCache.java
@@ -0,0 +1,144 @@
+package org.pinae.timon.cache.decorators.ehcache;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.pinae.timon.cache.AbstractCache;
+import org.pinae.timon.cache.CacheException;
+import org.pinae.timon.util.ObjectUtils;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.MemoryUnit;
+
+/**
+ * 基于ehcache的缓存
+ * 
+ * @author Huiyugeng
+ *
+ */
+public class EhCache extends AbstractCache {
+	
+	private static Logger logger = Logger.getLogger(EhCache.class);
+	
+	private static CacheManager Manager;
+
+	private EhCacheConfiguration config; // 缓存配置
+
+	private Cache cache;
+
+	public EhCache(String name, EhCacheConfiguration config) {
+		super(name);
+
+		String configUrl = config.getConfigUrl();
+		if (configUrl == null) {
+			
+			CacheConfiguration cacheConfig = new CacheConfiguration();
+			cacheConfig.setName(name);
+			cacheConfig.setMemoryStoreEvictionPolicy(config.getEvictionPolicy());
+			cacheConfig.setMaxEntriesLocalHeap(config.getMaxHeapSize());
+			
+			if (config.getMaxMemorySize() > 0) {
+				cacheConfig.setMaxBytesLocalHeap(MemoryUnit.BYTES.toBytes(config.getMaxMemorySize()));
+			}
+			if (config.getExpire() > 0) {
+				cacheConfig.timeToLiveSeconds(config.getExpire());
+			}
+			if (EhCache.Manager == null) {
+				EhCache.Manager = new CacheManager();
+			}
+			this.cache = new Cache(cacheConfig);
+			
+			EhCache.Manager.addCache(this.cache);
+		} else {
+			EhCache.Manager = CacheManager.newInstance(configUrl);
+			this.cache = EhCache.Manager.getCache(name);
+		}
+		
+		this.config = config;
+	}
+
+	public org.pinae.timon.cache.CacheConfiguration getCacheConfig() {
+		return config;
+	}
+
+	public void put(String key, Object value) throws CacheException {
+		put(key, value, this.config.getExpire());
+	}
+	
+	public void put(String key, Object value, int expire) throws CacheException {
+		
+		if (key == null) {
+			throw new NullPointerException("cache key can't be null");
+		}
+		if (value == null) {
+			throw new NullPointerException("cache value can't be null");
+		}
+		
+		if (key != null && value != null) {
+			
+			Element element = new Element(key, value);
+			if (expire > 0) {
+				element.setTimeToLive(expire);
+			}
+			long objectSize = ObjectUtils.size(element);
+			
+			if (config.getMaxObjectSize() == 0 || objectSize <= config.getMaxObjectSize()) {
+				cache.put(element);
+				
+				info.incPuts();
+				info.setSize(cache.getSize());
+				info.setMemorySize(cache.getMemoryStoreSize());
+			} else {
+				logger.info(String.format("Object size over max_object_size : key=%s, object_size=%d, max_object_size=%d", 
+						StringUtils.abbreviate(key, 15), objectSize, config.getMaxObjectSize()));
+			}
+		}
+		
+	}
+
+	public Object get(String key) throws CacheException {
+		Object value = null;
+		if (key != null) {
+			value = cache.get(key);
+		}
+		if (value != null) {
+			info.incHits();
+		} else {
+			info.incMisses();
+		}
+		
+		if (value != null && value instanceof Element) {
+			value = ((Element)value).getObjectValue();
+		}
+		return value;
+	}
+
+	public boolean remove(String key) throws CacheException {
+		if (key != null) {
+			
+			if (cache.remove(key)) {
+				
+				info.incRemoves();
+				info.setSize(cache.getSize());
+				info.setMemorySize(cache.getMemoryStoreSize());
+				
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void clear() throws CacheException {
+		cache.removeAll();
+		info.setSizeToZero();
+	}
+
+	public void close() throws CacheException {
+		Manager.removeCache(cache.getName());
+	}
+
+
+
+}
---
+++ src/main/java/org/pinae/timon/cache/decorators/ehcache/EhCacheConfiguration.java
@@ -0,0 +1,77 @@
+package org.pinae.timon.cache.decorators.ehcache;
+
+import org.pinae.timon.cache.CacheConfiguration;
+import org.pinae.timon.util.ConfigMap;
+
+/**
+ * 基于Ehcache的缓存配置
+ * 
+ * @author Huiyugeng
+ *
+ */
+public class EhCacheConfiguration extends CacheConfiguration {
+	
+	private String configUrl = null; // 配置文件URL
+	
+	private String evictionPolicy; // 对象回收策略
+	
+	/**
+	 * 构造函数
+	 */
+	public EhCacheConfiguration() {
+		super();
+	}
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param config 配置条目
+	 * 
+	 */
+	public EhCacheConfiguration(ConfigMap<String, String> config) {
+		super(config);
+		
+		this.configUrl = config.get("cache.ehcache.config_url");
+		this.evictionPolicy = config.getString("cache.ehcache.eviction", "LFU").toUpperCase();
+	}
+
+	/**
+	 * 返回配置文件URL
+	 * 
+	 * @return 配置文件URL
+	 */
+	public String getConfigUrl() {
+		return configUrl;
+	}
+
+	/**
+	 * 设置配置文件URL
+	 * 
+	 * @param configUrl 配置文件URL
+	 */
+	public void setConfigUrl(String configUrl) {
+		this.configUrl = configUrl;
+	}
+
+	/**
+	 * 返回对象回收策略
+	 * 
+	 * @return 对象回收策略
+	 */
+	public String getEvictionPolicy() {
+		if (evictionPolicy == null) {
+			return "LFU";
+		}
+		return evictionPolicy;
+	}
+
+	/**
+	 * 设置对象回收策略
+	 * 
+	 * @param evictionPolicy 对象回收策略 (LFU / LRU / FIFO / CLOCK )
+	 */
+	public void setEvictionPolicy(String evictionPolicy) {
+		this.evictionPolicy = evictionPolicy;
+	}	
+
+}
